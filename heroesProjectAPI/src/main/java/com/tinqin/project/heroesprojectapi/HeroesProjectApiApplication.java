925dddc3346f4dcfa0e1fba0268c1377215188f3
hakanihat <62997624+hakanihat@users.noreply.github.com>
2022-08-14 14:36:51 +0300

    Api project init

---
+++ heroesProjectAPI/src/main/java/com/tinqin/project/heroesprojectapi/HeroesProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.tinqin.project.heroesprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroesProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroesProjectApiApplication.class, args);
+	}
+
+}
---
+++ heroesProjectAPI/src/test/java/com/tinqin/project/heroesprojectapi/HeroesProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinqin.project.heroesprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
